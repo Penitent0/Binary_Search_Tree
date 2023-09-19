aeb4147b5bd7096a589df0da41100b2a74145892
Ken Lenhart <omnipaid@gmail.com>
2023-09-19 14:23:49 -0700

    Add tests for insert method

--- spec/binary_search_tree_spec.rb
+++ spec/binary_search_tree_spec.rb
@@ -3,9 +3,18 @@ require './lib/binary_search_tree'
 RSpec.describe BinarySearchTree do 
   let(:tree) { BinarySearchTree.new }
 
-  context 'on creation it' do
+  context 'on creation' do
     it 'instantiates' do
       expect(tree).to be_a(BinarySearchTree)
+    end
+  end
+
+  context 'has methods for' do
+    it 'insert data into node' do
+      expect(tree.insert(61, "Bill & Ted's Excellent Adventure")).to eq(0)
+      expect(tree.insert(16, "Johnny English")).to eq(1)
+      expect(tree.insert(92, "Sharknado 3")).to eq(1)
+      expect(tree.insert(50, "Hannibal Buress: Animal Furnace")).to eq(2)
     end
   end
 end
