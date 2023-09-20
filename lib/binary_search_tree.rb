9e7ac9f7f43a2ea2faf9af53ed72f8057842d2c4
Ken Lenhart <omnipaid@gmail.com>
2023-09-20 11:35:14 -0700

    Add additional tests for node attributes

--- lib/binary_search_tree.rb
+++ lib/binary_search_tree.rb
@@ -1,12 +1,24 @@
 class BinarySearchTree 
   def initialize
-
+    root = Node.new
   end
 
   def insert(score, film)
+    # Nodes - A Node's depth in the tree will be the number of nodes it is away from the root node. 
     # Inserting data into the tree will require creating a node
     # Create a node class that instantiates with the score and film title
     # nodes should point left toward lower film score and right toward higher film score
     # This method will instantiate an instance of Node and return the depth of the new node in the tree
+    # if root.left.blank? && root.right.blank?
+    #   root.score = score
+    #   root.film = film
+    # elsif root.score < score
+    #   root.left.score = score
+    #   root.left.film = film 
+    # elsif root.score > score 
+    #   root.right.score = score 
+    #   root.right.film = film
+    # end
+    
   end
 end
--- spec/binary_search_tree_spec.rb
+++ spec/binary_search_tree_spec.rb
@@ -1,7 +1,12 @@
 require './lib/binary_search_tree'
+require './lib/node.rb'
 
 RSpec.describe BinarySearchTree do 
   let(:tree) { BinarySearchTree.new }
+  let(:node_1) { Node.new(61, "Bill & Ted's Excellent Adventure") }
+  let(:node_2) { Node.new(16, "Johnny English") }
+  let(:node_3) { Node.new(92, "Sharknado 3") }
+  let(:node_4) { Node.new(50, "Hannibal Buress: Animal Furnace") }
 
   context 'on creation' do
     it 'instantiates' do
